bf925cbb5b818d851d48aa113c74defe7247aef0
Krfld <rodrigofldias@gmail.com>
2021-12-25 02:50:32 +0000

    .

--- ESP32_MIDI/src/main.cpp
+++ ESP32_MIDI/src/main.cpp
@@ -6,7 +6,9 @@ void setup()
 {
 	log_i("Setup");
 
-	log_i("Expander: %d", Devices::addExpander({0, 22, 21, 0x20}));
+	//! Something wrong with i2c
+
+	log_i("Expander: %d", Devices::addExpander({0, 19, 18, 0x20}));
 
 	log_i("Led: %d", Devices::addLed({0, 0, 7}));
 
@@ -17,8 +19,8 @@ void loop()
 {
 	log_i("Led on");
 	Devices::_leds[0]->set(On);
-	delay(500);
+	delay(1000);
 	log_i("Led off");
 	Devices::_leds[0]->set(Off);
-	delay(500);
+	delay(1000);
 }
