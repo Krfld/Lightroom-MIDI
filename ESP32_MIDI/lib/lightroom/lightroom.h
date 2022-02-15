c56b06be01b9139efd1152913e2bcc86b579a46d
Krfld <rodrigofldias@gmail.com>
2022-02-15 00:52:05 +0000

    .

--- ESP32_MIDI/lib/lightroom/lightroom.h
+++ ESP32_MIDI/lib/lightroom/lightroom.h
@@ -5,6 +5,16 @@
 
 //#include midi
 
+#define BASIC "Basic"					  // 1
+#define TONE_CURVE "Tone Curve"			  // 2
+#define HSL_COLOR "HSL/Color"			  // 3
+#define COLOR_GRADING "Color Grading"	  // 4
+#define DETAIL "Detail"					  // 5
+#define LENS_CORRECTION "Lens Correction" // 6
+#define TRANSFORM "Transform"			  // 7
+#define EFFECTS "Effects"				  // 8
+#define CALIBRATION "Calibration"		  // 9
+
 class Lightroom
 {
 private:
